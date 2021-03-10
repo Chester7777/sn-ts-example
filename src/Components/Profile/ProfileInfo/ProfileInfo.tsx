5d989d7a9f1d2853b82f670898c6cf804fdc9cf5
ches- <Es18.03.88@gmail.com>
2021-03-10 23:55:39 +0300

    class

--- src/Components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/Components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -10,7 +10,7 @@ const ProfileInfo = () => {
             <div className={s.image}>
                 <img
                     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzr0R9ziCVWDGq7tD8jO-FhKrY_KxfqpxW1g&usqp=CAU"
-                    alt="content"/>
+                    className={s.image_backgraund}/>
             </div>
             <div className={s.description}>
                <span>ava + description</span>
